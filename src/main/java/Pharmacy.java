3fe3db5a6678c60bfcdaaf037272e73931a9f8de
jinocencio-solvd <jinocencio.laba@solvd.com>
2023-04-16 11:18:21 -0700

    fixed formatting

--- src/main/java/Pharmacy.java
+++ src/main/java/Pharmacy.java
@@ -1,98 +1,86 @@
 /**
- * The Pharmacy class represents a pharmacy that has a name, phone number, email address, and an array of specialties.
+ * The Pharmacy class represents a pharmacy that has a name, phone number,
+ * email address, and an array of specialties.
  */
 public class Pharmacy {
-  private String name;
-  private String phoneNumber;
-  private String emailAddress;
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
 
-  /**
-   * Default constructor with no arguments
-   */
-  public Pharmacy() {
-    this.name = "defaultPharmacyName";
-    this.phoneNumber = "123-456-7890";
-    this.emailAddress = "defaultEmailAddress@email.com";
-  }
+    /**
+     * Default constructor with no arguments
+     */
+    public Pharmacy() {
+        this.name = "defaultPharmacyName";
+        this.phoneNumber = "123-456-7890";
+        this.emailAddress = "defaultEmailAddress@email.com";
+    }
 
-  /**
-   * Constructs a new Pharmacy object with the specified name, phone number, email address, and specialties.
-   *
-   * @param name         the name of the pharmacy
-   * @param phoneNumber  the phone number of the pharmacy
-   * @param emailAddress the email address of the pharmacy
-   * @param specialties  an array of specialties offered by the pharmacy
-   */
-  public Pharmacy(String name, String phoneNumber, String emailAddress, String[] specialties) {
-    this.name = name;
-    this.phoneNumber = phoneNumber;
-    this.emailAddress = emailAddress;
-  }
+    /**
+     * Constructs a new Pharmacy object with the specified name, phone
+     * number, email address, and specialties.
+     *
+     * @param name         the name of the pharmacy
+     * @param phoneNumber  the phone number of the pharmacy
+     * @param emailAddress the email address of the pharmacy
+     */
+    public Pharmacy(String name, String phoneNumber, String emailAddress) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
 
-  /**
-   * Constructs a new Pharmacy object with the specified name, phone number, email address, and specialties.
-   *
-   * @param name         the name of the pharmacy
-   * @param phoneNumber  the phone number of the pharmacy
-   * @param emailAddress the email address of the pharmacy
-   */
-  public Pharmacy(String name, String phoneNumber, String emailAddress) {
-    this.name = name;
-    this.phoneNumber = phoneNumber;
-    this.emailAddress = emailAddress;
-  }
+    /**
+     * Returns the name of the pharmacy.
+     *
+     * @return the name of the pharmacy
+     */
+    public String getName() {
+        return name;
+    }
 
-  /**
-   * Returns the name of the pharmacy.
-   *
-   * @return the name of the pharmacy
-   */
-  public String getName() {
-    return name;
-  }
+    /**
+     * Sets the name of the pharmacy
+     *
+     * @param name the new name of the pharmacy
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  /**
-   * Sets the name of the pharmacy
-   *
-   * @param name the new name of the pharmacy
-   */
-  public void setName(String name) {
-    this.name = name;
-  }
+    /**
+     * Returns the phone number of the pharmacy.
+     *
+     * @return the phone number of the pharmacy
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-  /**
-   * Returns the phone number of the pharmacy.
-   *
-   * @return the phone number of the pharmacy
-   */
-  public String getPhoneNumber() {
-    return phoneNumber;
-  }
+    /**
+     * Sets the phone number of the pharmacy.
+     *
+     * @param phoneNumber the new phone number of the pharmacy
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
-  /**
-   * Sets the phone number of the pharmacy.
-   *
-   * @param phoneNumber the new phone number of the pharmacy
-   */
-  public void setPhoneNumber(String phoneNumber) {
-    this.phoneNumber = phoneNumber;
-  }
+    /**
+     * Returns the email address of the pharmacy.
+     *
+     * @return the email address of the pharmacy
+     */
+    public String getEmailAddress() {
+        return emailAddress;
+    }
 
-  /**
-   * Returns the email address of the pharmacy.
-   *
-   * @return the email address of the pharmacy
-   */
-  public String getEmailAddress() {
-    return emailAddress;
-  }
-
-  /**
-   * Sets the email address of the pharmacy.
-   *
-   * @param emailAddress the new email address of the pharmacy
-   */
-  public void setEmailAddress(String emailAddress) {
-    this.emailAddress = emailAddress;
-  }
+    /**
+     * Sets the email address of the pharmacy.
+     *
+     * @param emailAddress the new email address of the pharmacy
+     */
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 }
--- src/test/java/PharmacyTest.java
+++ src/test/java/PharmacyTest.java
@@ -24,76 +24,84 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 
 class PharmacyTest {
-  Pharmacy defaultPharmacy;
-  Pharmacy noSpecialties;
-  Pharmacy pharmacy1;
+    Pharmacy defaultPharmacy;
+    Pharmacy noSpecialties;
+    Pharmacy pharmacy1;
 
 
-  @BeforeEach
-  void setUp() {
-    defaultPharmacy = new Pharmacy();
-    noSpecialties = new Pharmacy("noSpecialties", "000-000-0000", "noSpecialties@email.com");
-    pharmacy1 = new Pharmacy("pharmacy1", "111-111-1111", "pharmacy1@email.com");
-  }
+    @BeforeEach
+    void setUp() {
+        defaultPharmacy = new Pharmacy();
+        noSpecialties = new Pharmacy("noSpecialties", "000-000-0000",
+                "noSpecialties@email.com");
+        pharmacy1 = new Pharmacy("pharmacy1", "111-111-1111", "pharmacy1" +
+                "@email.com");
+    }
 
-  @Test
-  void getName() {
-    assertEquals("defaultPharmacyName", defaultPharmacy.getName());
-    assertEquals("pharmacy1", pharmacy1.getName());
-  }
+    @Test
+    void getName() {
+        assertEquals("defaultPharmacyName", defaultPharmacy.getName());
+        assertEquals("pharmacy1", pharmacy1.getName());
+    }
 
-  @Test
-  void setName() {
-    defaultPharmacy.setName("changedDefaultPharmacyName");
-    assertEquals("changedDefaultPharmacyName", defaultPharmacy.getName());
-    pharmacy1.setName("changedPharmacy1");
-    assertEquals("changedPharmacy1", pharmacy1.getName());
-  }
+    @Test
+    void setName() {
+        defaultPharmacy.setName("changedDefaultPharmacyName");
+        assertEquals("changedDefaultPharmacyName", defaultPharmacy.getName());
+        pharmacy1.setName("changedPharmacy1");
+        assertEquals("changedPharmacy1", pharmacy1.getName());
+    }
 
-  @Test
-  void getPhoneNumber() {
-    assertEquals("123-456-7890", defaultPharmacy.getPhoneNumber());
-    assertEquals("000-000-0000", noSpecialties.getPhoneNumber());
-    assertEquals("111-111-1111", pharmacy1.getPhoneNumber());
-  }
+    @Test
+    void getPhoneNumber() {
+        assertEquals("123-456-7890", defaultPharmacy.getPhoneNumber());
+        assertEquals("000-000-0000", noSpecialties.getPhoneNumber());
+        assertEquals("111-111-1111", pharmacy1.getPhoneNumber());
+    }
 
-  @Test
-  void setPhoneNumber() {
-    assertEquals("123-456-7890", defaultPharmacy.getPhoneNumber());
-    defaultPharmacy.setPhoneNumber("999-456-7890");
-    assertEquals("999-456-7890", defaultPharmacy.getPhoneNumber());
+    @Test
+    void setPhoneNumber() {
+        assertEquals("123-456-7890", defaultPharmacy.getPhoneNumber());
+        defaultPharmacy.setPhoneNumber("999-456-7890");
+        assertEquals("999-456-7890", defaultPharmacy.getPhoneNumber());
 
-    assertEquals("000-000-0000", noSpecialties.getPhoneNumber());
-    noSpecialties.setPhoneNumber("999-000-0000");
-    assertEquals("999-000-0000", noSpecialties.getPhoneNumber());
+        assertEquals("000-000-0000", noSpecialties.getPhoneNumber());
+        noSpecialties.setPhoneNumber("999-000-0000");
+        assertEquals("999-000-0000", noSpecialties.getPhoneNumber());
 
-    assertEquals("111-111-1111", pharmacy1.getPhoneNumber());
-    pharmacy1.setPhoneNumber("999-111-1111");
-    assertEquals("999-111-1111", pharmacy1.getPhoneNumber());
+        assertEquals("111-111-1111", pharmacy1.getPhoneNumber());
+        pharmacy1.setPhoneNumber("999-111-1111");
+        assertEquals("999-111-1111", pharmacy1.getPhoneNumber());
 
-  }
+    }
 
-  @Test
-  void getEmailAddress() {
-    assertEquals("defaultEmailAddress@email.com", defaultPharmacy.getEmailAddress());
-    assertEquals("noSpecialties@email.com", noSpecialties.getEmailAddress());
-    assertEquals("pharmacy1@email.com", pharmacy1.getEmailAddress());
-  }
+    @Test
+    void getEmailAddress() {
+        assertEquals("defaultEmailAddress@email.com",
+                defaultPharmacy.getEmailAddress());
+        assertEquals("noSpecialties@email.com",
+                noSpecialties.getEmailAddress());
+        assertEquals("pharmacy1@email.com", pharmacy1.getEmailAddress());
+    }
 
-  @Test
-  void setEmailAddress() {
-    assertEquals("defaultEmailAddress@email.com", defaultPharmacy.getEmailAddress());
-    defaultPharmacy.setEmailAddress("defaultEmailAddress@changedEmail.com");
-    assertEquals("defaultEmailAddress@changedEmail.com", defaultPharmacy.getEmailAddress());
+    @Test
+    void setEmailAddress() {
+        assertEquals("defaultEmailAddress@email.com",
+                defaultPharmacy.getEmailAddress());
+        defaultPharmacy.setEmailAddress("defaultEmailAddress@changedEmail.com");
+        assertEquals("defaultEmailAddress@changedEmail.com",
+                defaultPharmacy.getEmailAddress());
 
-    assertEquals("noSpecialties@email.com", noSpecialties.getEmailAddress());
-    noSpecialties.setEmailAddress("noSpecialties@changedEmail.com");
-    assertEquals("noSpecialties@changedEmail.com", noSpecialties.getEmailAddress());
+        assertEquals("noSpecialties@email.com",
+                noSpecialties.getEmailAddress());
+        noSpecialties.setEmailAddress("noSpecialties@changedEmail.com");
+        assertEquals("noSpecialties@changedEmail.com",
+                noSpecialties.getEmailAddress());
 
-    assertEquals("pharmacy1@email.com", pharmacy1.getEmailAddress());
-    pharmacy1.setEmailAddress("pharmacy1@changedEmail.com");
-    assertEquals("pharmacy1@changedEmail.com", pharmacy1.getEmailAddress());
-  }
+        assertEquals("pharmacy1@email.com", pharmacy1.getEmailAddress());
+        pharmacy1.setEmailAddress("pharmacy1@changedEmail.com");
+        assertEquals("pharmacy1@changedEmail.com", pharmacy1.getEmailAddress());
+    }
 
 
 }
