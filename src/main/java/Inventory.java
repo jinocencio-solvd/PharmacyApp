b00c7c4d9fee7588e7171308ce62f917409dbffa
jinocencio-solvd <jinocencio.laba@solvd.com>
2023-04-18 18:11:48 -0700

    updated Inventory class with inventory methods

--- src/main/java/Inventory.java
+++ src/main/java/Inventory.java
@@ -10,6 +10,62 @@ public class Inventory {
   public Inventory(ArrayList<Product> products) {
     this.products = products;
   }
+  public Inventory() {
+    this.products = new ArrayList<Product>();
+  }
+  /**
+   * Adds a new Product to the products inventory.
+   *
+   * @param newProduct the Product to add
+   */
+  public void addProductToInventory(Product newProduct) {
+    products.add(newProduct);
+  }
+
+  /**
+   * Removes a Product from the products inventory.
+   *
+   * @param productToRemove the Product to remove
+   */
+  public void removeProductFromInventory(Product productToRemove) {
+    products.remove(productToRemove);
+  }
+
+  /**
+   * Increases the quantity of the specified Product by the given amount.
+   *
+   * @param productName   the name of the Product to increase the quantity of
+   * @param quantityToAdd the amount to increase the quantity by
+   * @throws IllegalArgumentException if the specified Product is not found
+   */
+  public void increaseProductQuantity(String productName, int quantityToAdd) {
+    for (Product product : products) {
+      if (product.getName().equals(productName)) {
+        int currentQuantity = product.getQuantity();
+        product.setQuantity(currentQuantity + quantityToAdd);
+        return;
+      }
+    }
+    throw new IllegalArgumentException("Product not found: " + productName);
+  }
+
+  /**
+   * Decreases the quantity of the specified Product by the given amount.
+   *
+   * @param productName        the name of the Product to decrease the quantity of
+   * @param quantityToSubtract the amount to decrease the quantity by
+   * @throws IllegalArgumentException if the specified Product is not found
+   */
+  public void decreaseProductQuantity(String productName, int quantityToSubtract) {
+    for (Product product : products) {
+      if (product.getName().equals(productName)) {
+        int currentQuantity = product.getQuantity();
+        product.setQuantity(currentQuantity - quantityToSubtract);
+        return;
+      }
+    }
+    throw new IllegalArgumentException("Product not found: " + productName);
+  }
 
   public ArrayList<Product> getProducts() {
     return products;
@@ -21,8 +77,6 @@ public class Inventory {
 
   @Override
   public String toString() {
-    return "Inventory{" +
-        "products=" + products +
-        '}';
+    return "Inventory{" + "products=" + products + '}';
   }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,75 +2,73 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Address address0 = new Address("001 Pearl St", "Anytown", "CA", 12345);
-        Address address1 = new Address("123 Main St", "Anytown", "CA", 12345);
-        Address address2 = new Address("456 Elm St", "Anytown", "NY", 67890);
-        Address address3 = new Address("789 Oak St", "Sometown", "FL", 24680);
-        Address address4 = new Address("321 Pine St", "Othertown", "TX", 13579);
-        Address address5 = new Address("111 Maple St", "Another Town", "GA",
-                97531);
-        Address address6 = new Address("222 Cedar St", "Someplace", "VA",
-                86420);
+  public static void main(String[] args) {
+    Address address0 = new Address("001 Pearl St", "Anytown", "CA", 12345);
+    Address address1 = new Address("123 Main St", "Anytown", "CA", 12345);
+    Address address2 = new Address("456 Elm St", "Anytown", "NY", 67890);
+    Address address3 = new Address("789 Oak St", "Sometown", "FL", 24680);
+    Address address4 = new Address("321 Pine St", "Othertown", "TX", 13579);
+    Address address5 = new Address("111 Maple St", "Another Town", "GA",
+        97531);
+    Address address6 = new Address("222 Cedar St", "Someplace", "VA",
+        86420);
 
-        Pharmacist pharmacist1 = new Pharmacist("John Doe", "555-1234",
-                address1, "E001", "CA-12345");
-        Pharmacist pharmacist2 = new Pharmacist("Jane Smith", "555-5678",
-                address2, "E002", "NY-67890");
+    Pharmacist pharmacist1 = new Pharmacist("John Doe", "555-1234",
+        address1, "E001", "CA-12345");
+    Pharmacist pharmacist2 = new Pharmacist("Jane Smith", "555-5678",
+        address2, "E002", "NY-67890");
 
-        PharmacyTechnician tech1 = new PharmacyTechnician("John Tech", "555" +
-                "-1234", address3, "EMP-001", "CA123456");
-        tech1.setCashierTrained(true);
-        PharmacyTechnician tech2 = new PharmacyTechnician("Jane Tech", "555" +
-                "-5678", address4, "EMP-002", "CA654321");
+    PharmacyTechnician tech1 = new PharmacyTechnician("John Tech", "555" +
+        "-1234", address3, "EMP-001", "CA123456");
+    tech1.setCashierTrained(true);
+    PharmacyTechnician tech2 = new PharmacyTechnician("Jane Tech", "555" +
+        "-5678", address4, "EMP-002", "CA654321");
 
-        ArrayList<Employee> employees = new ArrayList<>();
-        employees.add(pharmacist1);
-        employees.add(pharmacist2);
-        employees.add(tech1);
-        employees.add(tech2);
+    ArrayList<Employee> employees = new ArrayList<>();
+    employees.add(pharmacist1);
+    employees.add(pharmacist2);
+    employees.add(tech1);
+    employees.add(tech2);
 
-        Item product1 = new Item("Band-Aids", 2.99, 50);
-        Item product2 = new Item("Hydrogen Peroxide", 1.99, 25);
-        Item product3 = new Item("Antacid Tablets", 4.99, 30);
-        Item product4 = new Item("Cough Drops", 3.49, 40);
-        Medication medication1 = new Medication("Aspirin", "500 mg", 0.10, 100);
-        Medication medication2 = new Medication("Ibuprofen", "200 mg", 0.08,
-                50);
-        Medication medication3 = new Medication("Acetaminophen", "325 mg",
-                0.05, 200);
-        Medication medication4 = new Medication("Naproxen", "220 mg", 0.12, 75);
+    // Create products
+    Item product1 = new Item("Band-Aids", 2.99, 50);
+    Item product2 = new Item("Hydrogen Peroxide", 1.99, 25);
+    Item product3 = new Item("Antacid Tablets", 4.99, 30);
+    Item product4 = new Item("Cough Drops", 3.49, 40);
+    Medication medication1 = new Medication("Aspirin", "500 mg", 0.10, 100);
+    Medication medication2 = new Medication("Ibuprofen", "200 mg", 0.08,
+        50);
+    Medication medication3 = new Medication("Acetaminophen", "325 mg",
+        0.05, 200);
+    Medication medication4 = new Medication("Naproxen", "220 mg", 0.12, 75);
 
+    // Add to inventory
+    Inventory inventory = new Inventory();
+    inventory.addProductToInventory(product1);
+    inventory.addProductToInventory(product2);
+    inventory.addProductToInventory(product3);
+    inventory.addProductToInventory(product4);
+    inventory.addProductToInventory(medication1);
+    inventory.addProductToInventory(medication2);
+    inventory.addProductToInventory(medication3);
+    inventory.addProductToInventory(medication4);
 
+    Pharmacy pharmacy = new Pharmacy("Joffrey's Pharmacy", address0, "123" +
+        "-321-4567"
+        , "pharmEmail@email.com", inventory, employees);
 
-        ArrayList<Product> products = new ArrayList<>();
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
-        products.add(product4);
-        products.add(medication1);
-        products.add(medication2);
-        products.add(medication3);
-        products.add(medication4);
+    Patient patient1 = new Patient("Tom Davis", "555-7890", address5,
+        1005, "Humana");
+    Patient patient2 = new Patient("Sara Johnson", "555-2345", address6,
+        1006, "Anthem");
 
-        Inventory inventory = new Inventory(products);
+    Prescription prescription1 = new Prescription("RX123", 2, false,
+        medication1, patient1);
 
-        Pharmacy pharmacy = new Pharmacy("Joffrey's Pharmacy", address0, "123" +
-                "-321-4567"
-                , "pharmEmail@email.com", inventory, employees);
+    Prescription prescription2 = new Prescription("RX456", 1, true,
+        medication2, patient2);
 
-        Patient patient1 = new Patient("Tom Davis", "555-7890", address5,
-                1005, "Humana");
-        Patient patient2 = new Patient("Sara Johnson", "555-2345", address6,
-                1006, "Anthem");
-
-        Prescription prescription1 = new Prescription("RX123", 2, false,
-                medication1, patient1);
-
-        Prescription prescription2 = new Prescription("RX456", 1, true,
-                medication2, patient2);
-
-    }
+  }
 
 
 }
