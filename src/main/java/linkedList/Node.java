e7d3e7791dd4e2da5b043538d535571ce0e093c9
jinocencio-solvd <jinocencio.laba@solvd.com>
2023-04-28 18:43:46 -0700

    added generic Linkedlist

---
+++ src/main/java/linkedList/Node.java
@@ -0,0 +1,12 @@
+package linkedList;
+
+public class Node<T> {
+
+    protected T data;
+    protected Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
